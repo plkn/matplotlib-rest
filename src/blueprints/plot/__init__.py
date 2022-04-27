3744ac3d4dc0784a63fa4693514532df89961874
Pavel Koryakin <pavel.koryakin@gmail.com>
2022-04-28 01:31:15 +0300

    plot POST contract

--- src/blueprints/plot/__init__.py
+++ src/blueprints/plot/__init__.py
@@ -1,34 +1,33 @@
+# from bottle import route, request, run
+from textblob import TextBlob
 from flask import Blueprint, jsonify, request
+
+from src.schemas import PlotSchema
 
 plot_bp = Blueprint(name="plot_bp", import_name=__name__)
 
 
-@plot_bp.route('/test', methods=['GET'])
-def test():
+@plot_bp.route('/', methods=['POST'])
+def plot():
     """
     ---
-    get:
-      description: test endpoint
+    post:
+      description: matplotlib.plot endpoint
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: PlotSchema
       responses:
         '200':
           description: call successful
           content:
             application/json:
-              schema: OutputSchema
-      tags:
-          - testing
+              schema: LineSchema
     """
-    output = {"msg": "I'm the test endpoint from blueprint_x."}
+    plot_schema = PlotSchema()
+    plot_schema.load(request.json)
+
+    output = {"msg": "I'm the test endpoint from matplotlib.plot."}
     return jsonify(output)
 
-
-# add view function to the blueprint
-# @plot_bp.route('/plus', methods=['POST'])
-# def plus_x():
-#     # retrieve body data from input JSON
-#     data = request.get_json()
-#     in_val = data['number']
-#     # compute result and output as JSON
-#     result = in_val + x
-#     output = {"msg": f"Your result is: '{result}'"}
-#     return jsonify(output)

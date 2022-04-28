bc4392769335f6ea154f275dac1f79fd02700c83
Pavel Koryakin <pavel.koryakin@gmail.com>
2022-04-29 00:49:04 +0300

    figure and subplot schemes

--- src/schemas/figure.py
+++ src/schemas/figure.py
@@ -1,8 +1,9 @@
-from marshmallow import Schema, fields, post_load
-
-
 class Figure:
-    def __init__(self, tight_layout=False, edgecolor='white', facecolor='white'):
+    def __init__(self, subplots, tight_layout=False, edgecolor='white', facecolor='white', subplot_columns=1, subplot_rows=1):
         self.edgecolor = edgecolor
         self.facecolor = facecolor
         self.tight_layout = tight_layout
+        self.subplot_columns = subplot_columns
+        self.subplot_rows = subplot_rows
+        self.subplots = subplots
+
---
+++ src/schemas/subplot.py
@@ -0,0 +1,5 @@
+class Subplot:
+    def __init__(self, plot, index=1):
+        self.plot = plot
+        self.index = index
+

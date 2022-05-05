e88c9bc24cb44c733a7d45ff93bb810de7b9c727
Pavel Koryakin <pavel@codestetic.com>
2022-05-05 17:43:04 +0300

    Figure and subplot schemas

--- src/schemas/figure.py
+++ src/schemas/figure.py
@@ -1,5 +1,11 @@
+from marshmallow import Schema, fields, post_load
+
+from src.schemas.subplot import SubplotSchema
+
+
 class Figure:
-    def __init__(self, subplots, tight_layout=False, edgecolor='white', facecolor='white', subplot_columns=1, subplot_rows=1):
+    def __init__(self, subplots, tight_layout=False, edgecolor='white', facecolor='white', subplot_columns=1,
+                 subplot_rows=1):
         self.edgecolor = edgecolor
         self.facecolor = facecolor
         self.tight_layout = tight_layout
@@ -7,3 +13,15 @@ class Figure:
         self.subplot_rows = subplot_rows
         self.subplots = subplots
 
+
+class FigureSchema(Schema):
+    edgecolor = fields.String(description="Edge color", required=False, load_default="white")
+    facecolor = fields.String(description="Face color", required=False, load_default="white")
+    subplot_columns = fields.Integer(description="Number of columns of suplots grid", required=False, load_default=1)
+    subplot_rows = fields.Integer(description="Number of rows of suplots grid", required=False, load_default=1)
+    tight_layout = fields.Bool(description="Tight layout switch", required=False, load_default=False)
+    subplots = fields.List(fields.Nested(SubplotSchema))
+
+    @post_load
+    def load_figure(self, data, **kwargs):
+        return Figure(**data)
--- src/schemas/subplot.py
+++ src/schemas/subplot.py
@@ -1,5 +1,11 @@
+from marshmallow import Schema, fields
+
+
 class Subplot:
     def __init__(self, plot, index=1):
         self.plot = plot
         self.index = index
 
+
+class SubplotSchema(Schema):
+    lie = 1
